c6c4a40e0d52baa77560b3c57c63a75aee770ce2
Davide Petilli <7h3.k3rn31@gmail.com>
2016-06-07 14:37:38 +0200

    Move skebby_file.rb in skebby subdir

---
+++ lib/ztos/skebby/skebby_file.rb
@@ -0,0 +1,39 @@
+# The MIT License (MIT)
+# Copyright (c) 2016 Davide Petilli
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom
+# the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+module Skebby
+  # The class defining the CSV to import on Skebby
+  class SkebbyFile < File
+    # Creates a file that when initializes, adds a header.
+    #
+    # Arguments:
+    #   filename: (String)
+    #   mode: (String)
+
+    def initialize(filename, mode)
+      super(filename, mode)
+      # Every time we create a ne file, we insert
+      # the CSV header in the Skebby format.
+      # Again we only use the columns we need.
+      puts 'nome;cognome;email;numero di cellulare'
+    end
+  end
+end
